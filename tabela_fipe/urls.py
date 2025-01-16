1e3e263e6f77c78c4e24f3b7de391348781bcab6
FerrariGuilherme <guilherme.ferrari@sp.gov.br>
2025-01-16 16:42:21 -0300

    mod url

--- tabela_fipe/urls.py
+++ tabela_fipe/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('consulta', include('consulta.urls'))
+    path('', include('consulta.urls'))
 ]
